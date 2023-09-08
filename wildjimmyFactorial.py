30443d4456af75dc6298140db5ebca6ee9eed021
WildJimmy <94491866+WildJimmy@users.noreply.github.com>
2023-09-08 07:55:08 -0400

    Added documentation explaining the function's utility

--- wildjimmyFactorial.py
+++ wildjimmyFactorial.py
@@ -1,3 +1,6 @@
+#This function takes an integer num and returns num facotrial (num!). I.e. num*(num-1)*(num-2)...
+#Warning: may take a while to run if you throw in a huge number!
+
 def factorial(num):
     
     fact = 1
